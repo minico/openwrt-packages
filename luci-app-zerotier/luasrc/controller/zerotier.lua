19943adcb6913577eb10ca1aa529e31de78e880d
minico <xj.zhang@msn.com>
2022-02-05 14:48:36 +0800

    move zerotier to services menu

--- luci-app-zerotier/luasrc/controller/zerotier.lua
+++ luci-app-zerotier/luasrc/controller/zerotier.lua
@@ -5,16 +5,14 @@ function index()
 return
 end
 
-entry({"admin","vpn"}, firstchild(), "VPN", 45).dependent = false
-
-local page = entry({"admin", "vpn", "zerotier"},firstchild(), _("ZeroTier"))
+local page = entry({"admin", "services", "zerotier"},firstchild(), _("ZeroTier"))
 page.dependent = false
 page.acl_depends = { "luci-app-zerotier" }
 
-entry({"admin", "vpn", "zerotier", "general"},cbi("zerotier/settings"), _("Base Setting"), 1)
-entry({"admin", "vpn", "zerotier", "log"},form("zerotier/info"), _("Interface Info"), 2)
+entry({"admin", "services", "zerotier", "general"},cbi("zerotier/settings"), _("Base Setting"), 1)
+entry({"admin", "services", "zerotier", "log"},form("zerotier/info"), _("Interface Info"), 2)
 
-entry({"admin","vpn","zerotier","status"},call("act_status"))
+entry({"admin","services","zerotier","status"},call("act_status"))
 end
 
 function act_status()
